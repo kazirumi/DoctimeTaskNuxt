c54e52f16964260ab2b93114d7e77f3df67fad50
kazirumi <tangorumi@gmail.com>
2024-02-16 21:42:10 +0600

    Unwanted code removed

--- components/Modal/Modal.vue
+++ components/Modal/Modal.vue
@@ -6,21 +6,15 @@
                 <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                     <div
                         class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 w-full sm:max-w-2xl sm:p-6 md:max-w-2xl">
-                        <div class="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
-                            <!-- <button @click="close" type="button" class="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
-                            <span class="sr-only">Close</span>
-                            <CrossIcon />
-                        </button> -->
-                        </div>
 
                         <slot />
 
                         <div class="mt-4 sm:mt-4 sm:flex sm:flex-row-reverse">
-                            <!-- {#if submit != false} -->
+
                             <button type="button"
                                 class="inline-flex w-full justify-center py-2 text-sm sm:ml-3 sm:w-auto primary-btn"
                                 @click="submit">Save</button>
-                            <!-- {/if} -->
+
                             <button type="button"
                                 class="mt-3 inline-flex w-full justify-center py-2 text-sm sm:mt-0 sm:w-auto primary-btn bg-red-600 hover:bg-red-500"
                                 @click="close">Cancel</button>
